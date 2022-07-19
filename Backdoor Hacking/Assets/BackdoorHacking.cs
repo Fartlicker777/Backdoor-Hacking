e24a915fb53adcf6c32046aab4e34ce9b7c1c181
Brendan King <brendan@brendanking.com>
2022-07-19 12:03:39 -0400

    Fix thing that was bugging me

--- Backdoor Hacking/Assets/BackdoorHacking.cs
+++ Backdoor Hacking/Assets/BackdoorHacking.cs
@@ -1094,10 +1094,7 @@ public class BackdoorHacking : MonoBehaviour {
          }
       }
       if (CurrentState == HackState.Idle || CurrentState == HackState.GettingHacked) {
-         if (DOSCoinAmount >= 100)
-            CurrentVBucks.text = DOSCoinAmount.ToString("000.000");
-         else
-            CurrentVBucks.text = DOSCoinAmount.ToString("00.000");
+         CurrentVBucks.text = DOSCoinAmount.ToString("00.000");
          return;
       }
       else if (CurrentState == HackState.ZoneWall) {
